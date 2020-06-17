5fc820aa89b85da4fb258b187af6855dce13b6c1
Urvesh <urveshpatel3999@gmail.com>
2020-06-17 18:53:55 +0530

    Add files via upload

---
+++ List Comprehension.py
@@ -0,0 +1,8 @@
+#list comprehension
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+print([[a,b,c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if((a+b+c) != n)])
+
